0ea120dfbc16e3000e3f02934239fc9a3689ade9
Mykhailo Madzhara <2madzhara@gmail.com>
2021-08-05 16:17:37 +0300

    Adds prettier as code formater

--- src/Components/Layout/Basket/Basket.js
+++ src/Components/Layout/Basket/Basket.js
@@ -1,28 +1,36 @@
-import React from 'react'
-import BasketGoods from './BacketGoods/BasketGoods'
-import classes from './Basket.css'
+import React from "react";
+import BasketGoods from "./BacketGoods/BasketGoods";
+import classes from "./Basket.css";
 
-const Bascket = props => {
-    
-    let result = 0
-    for (let i = 0; i < props.state.cart.length; i++) {
-        if (props.state.cart[i].productId === props.state.products[props.state.cart[i].productId - 1].id) {
-            if (props.state.cart[i].count >= 0) {
-            result = result +  props.state.products[props.state.cart[i].productId - 1].price * props.state.cart[i].count}
-        } 
+const Bascket = (props) => {
+  let result = 0;
+  for (let i = 0; i < props.state.cart.length; i++) {
+    if (
+      props.state.cart[i].productId ===
+      props.state.products[props.state.cart[i].productId - 1].id
+    ) {
+      if (props.state.cart[i].count >= 0) {
+        result =
+          result +
+          props.state.products[props.state.cart[i].productId - 1].price *
+            props.state.cart[i].count;
+      }
     }
+  }
 
-    return (
-        <div className={classes.Basket}>
-            <p> Products in the basket: &nbsp;
-                <span>{ props.state.cart.length}</span> 
-            </p>
-            <BasketGoods 
-                state={props.state}
-            />
-            <p>Cost: <span>{result}</span> </p>
-        </div>
-    )
-}
+  return (
+    <div className={classes.Basket}>
+      <p>
+        {" "}
+        Products in the basket: &nbsp;
+        <span>{props.state.cart.length}</span>
+      </p>
+      <BasketGoods state={props.state} />
+      <p>
+        Cost: <span>{result}</span>{" "}
+      </p>
+    </div>
+  );
+};
 
-export default Bascket
+export default Bascket;
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,105 +1,97 @@
-import React, { Component } from 'react'
-import Products from './Products/Products'
-import Basket from './Basket/Basket'
-import classes from './Layout.css'
+import React, { Component } from "react";
+import Products from "./Products/Products";
+import Basket from "./Basket/Basket";
+// import classes from './Layout.css'
+
 // import products from '../../products.json'
 
-
 class Layout extends Component {
-    
-    
+  state = {
+    products: [],
+    loadedProducts: false,
+    cart: [],
+    loadedCart: false,
+  };
 
-    state={
-        products:[],
-        loadedProducts: false,
-        cart: [],
-        loadedCart: false 
-    }
-    
-    componentDidMount() {
-        fetch('state/products.json')
-        .then( res => res.json())
-        .then(
-            (result) => {
-                this.setState({
-                    products: result,
-                    loadedProducts: true
-                })
-            }
-        )
-        fetch('state/cart.json')
-        .then( res => res.json())
-        .then(
-            (result) => {
-                this.setState({
-                    cart: result,
-                    loadedCart: true
-                })
-            }
-        )
-    }
-
-    changeCountHandler = (value, productId) => {
-        let updatedCartArray = []
-        
-        for (let i = 0; i < this.state.cart.length; ++i) {
-            updatedCartArray.push(this.state.cart[i])
-            if (this.state.cart[i].productId === productId) {
-                updatedCartArray[i].count = updatedCartArray[i].count + value
-            }
-        }
-        for (let i = 0; i < updatedCartArray.length; i++) {
-            if (updatedCartArray[i].count <= 0) {
-                updatedCartArray.splice(i, 1)
-            }
-        }
-
+  componentDidMount() {
+    fetch("state/products.json")
+      .then((res) => res.json())
+      .then((result) => {
         this.setState({
-            ...this.state,
-            cart: updatedCartArray, 
-        })
+          products: result,
+          loadedProducts: true,
+        });
+      });
+
+    fetch("state/cart.json")
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          cart: result,
+          loadedCart: true,
+        });
+      });
+  }
+
+  changeCountHandler = (value, productId) => {
+    let updatedCartArray = [];
+
+    for (let i = 0; i < this.state.cart.length; ++i) {
+      updatedCartArray.push(this.state.cart[i]);
+      if (this.state.cart[i].productId === productId) {
+        updatedCartArray[i].count = updatedCartArray[i].count + value;
+      }
     }
-    
-    addProductHandler = (productId) => {
-        this.setState ({
-            ...this.state,
-            cart: [
-                ...this.state.cart,
-                {
-                    productId: productId,
-                    count: 1,
-                }
-            ],
-        })
+    for (let i = 0; i < updatedCartArray.length; i++) {
+      if (updatedCartArray[i].count <= 0) {
+        updatedCartArray.splice(i, 1);
+      }
     }
-        
-    render() {
-        this.state.cart.sort(function (a, b) {
-            if (a.productId > b.productId) {
-                return 1
-            }
-            if (a.productId < b.productId) {
-                return -1
-            }
-            return 0
-        })
-        return (
-            <React.Fragment>
-                {this.state.loadedProducts && this.state.loadedCart
-                ? <React.Fragment>
-                    <Products 
-                        onClick={this.changeCountHandler}
-                        state={this.state}
-                        onAdd={this.addProductHandler}
-                    />
-                    <Basket 
-                        state={this.state}
-                    />  
-                  </React.Fragment>
-                : null}
-            </React.Fragment>
-        )
-    }
+
+    this.setState({
+      ...this.state,
+      cart: updatedCartArray,
+    });
+  };
+
+  addProductHandler = (productId) => {
+    this.setState({
+      ...this.state,
+      cart: [
+        ...this.state.cart,
+        {
+          productId: productId,
+          count: 1,
+        },
+      ],
+    });
+  };
+
+  render() {
+    this.state.cart.sort(function (a, b) {
+      if (a.productId > b.productId) {
+        return 1;
+      }
+      if (a.productId < b.productId) {
+        return -1;
+      }
+      return 0;
+    });
+    return (
+      <React.Fragment>
+        {this.state.loadedProducts && this.state.loadedCart ? (
+          <React.Fragment>
+            <Products
+              onClick={this.changeCountHandler}
+              state={this.state}
+              onAdd={this.addProductHandler}
+            />
+            <Basket state={this.state} />
+          </React.Fragment>
+        ) : null}
+      </React.Fragment>
+    );
+  }
 }
 
-export default Layout
+export default Layout;
